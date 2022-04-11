928c8db24babc36bbe93761cc2fff968562b3127
Teerut26 <teerutsr@gmail.com>
2022-04-11 20:51:57 +0700

    add client type

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -3,7 +3,8 @@ import { Client } from "discord.js";
 module.exports = {
     name: "ready",
     once: true,
-    execute(client: any) {
-        console.log(client.user.username, "is Ready !");
+    execute(client: Client) {
+        console.log(client.uptime);
+        console.log(client.user?.username, "is Ready !");
     },
 };
