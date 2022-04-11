e47fc78ea49fc5018b50baf8b1981e65f94f0257
Teerut26 <teerutsr@gmail.com>
2022-04-11 20:52:39 +0700

    de console.log(client.uptime);

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -4,7 +4,6 @@ module.exports = {
     name: "ready",
     once: true,
     execute(client: Client) {
-        console.log(client.uptime);
         console.log(client.user?.username, "is Ready !");
     },
 };
